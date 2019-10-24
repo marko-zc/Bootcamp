29d86d93730b20b3a2f6f12a1cb63f2def449e51
marko-zc <marko.zarkovic.cerneli@gmail.com>
2019-10-24 12:47:36 +0200

    add functions problem set exercise

---
+++ JS Basics/FunctionsProblemSet.js
@@ -0,0 +1,18 @@
+function isEven(num) {
+    return num % 2 === 0;
+}
+
+function factorial(num) {
+
+    var result = 1;
+    for (var i = 2; i <= num; i++) {
+        result *= i;
+        
+    }
+    return result;
+}
+
+function kebabToSnake(str) {
+    var newStr = str.replace(/-/g,"_");
+    return newStr;
+}

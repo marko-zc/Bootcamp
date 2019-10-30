3893a7b6ea18f40b0a874f67bf657b44d0576e38
marko-zc <marko.zarkovic.cerneli@gmail.com>
2019-10-30 19:00:10 +0100

    update todo list exercise

--- JS Basics/list.js
+++ JS Basics/list.js
@@ -1,7 +1,7 @@
 window.setTimeout(function() {
 
 
-var todos = [];
+var todos = ["Buy New Turtle"];
 
 var input = prompt("What would you like to do?");
 
@@ -10,23 +10,40 @@ var input = prompt("What would you like to do?");
 while(input !== "quit") {
     //handle input
     if(input === "list") {
-        console.log(todos);
+        listTodos();
     } else if(input === "new") {
-        //ask for new todo
-        var newTodo = prompt("Enter new todo");
-        //add to todos array
-        todos.push(newTodo);
+        addTodo();
+    } else if (input === "delete") {
+       deleteTodo();
     }
     //ask again for new input
     input = prompt("What would you like to do?");
 }
 console.log("OK, YOU QUIT THE APP");
 
+function listTodos(){
+    console.log("**********")
+    todos.forEach(function(todo, i){
+        console.log(i + ": " + todo);
+    });
+    console.log("**********")
+}
 
+function addTodo(){
+     //ask for new todo
+     var newTodo = prompt("Enter new todo");
+     //add to todos array
+     todos.push(newTodo);
+     console.log("Added Todo");
+}
 
-
-
-
+function deleteTodo(){
+     //ask for index of todo to be deleted
+     var index = prompt("Enter index of todo to delete");
+     //delete that todo
+     todos.splice(index, 1);
+     console.log("Deleted Todo");
+}
 
 
 

876cffa664d85d0f99415db32e78449d6659feff
marko-zc <marko.zarkovic.cerneli@gmail.com>
2019-11-21 22:59:30 +0100

    update color game project

--- Color Game/colorGame.js
+++ Color Game/colorGame.js
@@ -8,8 +8,9 @@ var colors = [
 ]
 
 var squares = document.querySelectorAll(".square")
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -23,9 +24,24 @@ for(var i = 0; i < squares.length; i++){
         var clickedColor = this.style.backgroundColor;
         //compare color to pickedColor
         if (clickedColor === pickedColor) {
-            alert("Correct!");
+            messageDisplay.textContent = "Correct!";
+            changeColors(clickedColor);
         } else {
-            alert("Wrong!");
+            this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "Try Again";
         }
     });
+}
+
+function changeColors(color) {
+    //loop through all squares
+    for(var i = 0; i < colors.length; i++){
+    //change each color to match given color
+        squares[i].style.backgroundColor = color;
+    }
+}
+
+function pickColor(){
+    var random = Math.floor(Math.random() * colors.length)
+    return colors[random];
 }

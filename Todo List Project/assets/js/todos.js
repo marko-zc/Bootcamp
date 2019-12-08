0c49ee360ee8be9d120fa69661bd5d82a520cc10
marko-zc <marko.zarkovic.cerneli@gmail.com>
2019-12-08 19:11:31 +0100

    update todo list project

--- Todo List Project/assets/js/todos.js
+++ Todo List Project/assets/js/todos.js
@@ -20,3 +20,7 @@ $("input[type='text']").keypress(function(event){
         $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
     }
 });
+
+$("#toggle-form").click(function(){
+	$("input[type='text']").fadeToggle();
+});
